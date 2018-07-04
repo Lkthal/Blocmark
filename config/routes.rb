659fc3843836904834b90d1701ed8208cfedeb58
Thal Syhabout <tsyhabout@gmail.com>
2018-07-04 16:23:55 -0700

    add topic listing and CRUD function

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  get 'welcome/about'
+
+  resources :topics do
+ # #34
+     #resources :bookmarks, except: [:index]
+   end
   root 'welcome#index'
+  get 'about' => 'welcome#about'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
