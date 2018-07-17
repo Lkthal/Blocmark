928025aeb171f792131e0db63c976229f8e8a0a2
Thal Syhabout <tsyhabout@gmail.com>
2018-07-17 16:50:15 -0700

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'likes/index'
+  # get 'users/show'
+  # get 'likes/index'
   # get 'bookmarks/show'
   # get 'bookmarks/new'
   # get 'bookmarks/edit'
@@ -12,5 +13,6 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'about' => 'welcome#about'
   devise_for :users
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
