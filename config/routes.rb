7444cc11f3b9f2d1ce8e2137443f6cccaf001c43
Thal Syhabout <tsyhabout@gmail.com>
2018-07-18 01:46:55 -0700

    update merging errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
-  # get 'likes/index'
-=======
-  get 'likes/index'
->>>>>>> 669c3770873057054b42a379a59b9c57f1e03662
-  # get 'bookmarks/show'
-  # get 'bookmarks/new'
-  # get 'bookmarks/edit'
   resources :topics do
      resources :bookmarks, except: [:index] do
        resources :likes, only: [:index, :create, :destroy]
@@ -16,6 +8,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'about' => 'welcome#about'
   devise_for :users
+  resources :users, only: [:show]
   post :incoming, to: 'incoming#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
